22d2bbdc1d28a34cd7d9f6354c4025e33ee4cf61
Rebecca Girndt <r.l.girndt@gmail.com>
2022-12-23 23:15:04 -0600

    created criteria confirms, validated all input, randomized password elements. really got into it today.

--- Develop/script.js
+++ Develop/script.js
@@ -1,53 +1,120 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-//04-activities 07/08
+// Writes password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.getElementById("password");
   passwordText.textContent = password;
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// asks the user questions and generates random password based on answers
 function generatePassword(){
   var lengthChoice = prompt("Choose the length of your password, between 8 - 128 characters.");
-    //validate user num
+    
+  //validates chosen character length
     if (lengthChoice == null) {
       return;
     } else if (lengthChoice >= 8 && lengthChoice <= 128) {
-      alert("That works!");
-      return lengthChoice;
+      alert("Please confirm at least one of the following 4 options.");
+      console.log(lengthChoice);
     } else {
       alert("Please type an integer between 8 and 128!");
-      generatePassword();
+      return generatePassword();
     }
-  //confirm user upper, lower, num, special chars (4 confirm prompts)
-    //4 vars to save t/f to include characters
-    //validate that atleast one cat was chosen
-  //vars that include all user options of each catagory. Array of strings for each cat.
-    //var upper = ["A", "B", etc...]
-  //if user says yes to include that cat choose random elements from the array
-    //how to decide how many elements to choose from each array 
-      //?combine arrays that are yessed into new array to randomly choose from
-  //validate that atleast one cat was chosen
-  //join chosen elements into a string
- 
-};
+    
+    var password = [];
+    //vars to save the t/f values for including characters
+    var lowerChoices = [];
+    var upperChoices = [];
+    var numberChoices = [];
+    var specialChoices = [];
 
-//must return a string value that is the password
-  //prompt user to choose num btwn 8 and 128
-    //var to save length
-    //validate user num
-  //confirm user upper, lower, num, special chars (4 confirm prompts)
-    //4 vars to save t/f to include characters
-    //validate that atleast one cat was chosen
-  //vars that include all user options of each catagory. Array of strings for each cat.
-    //var upper = ["A", "B", etc...]
-  //if user says yes to include that cat choose random elements from the array
-    //how to decide how many elements to choose from each array 
-      //?combine arrays that are yessed into new array to randomly choose from
-  //validate that atleast one cat was chosen
-  //join chosen elements into a string
+    //User confirms or denies upper, lower, num, and/or special characters (4 confirm prompts)
+    //if confirmed, the arrays are updated to include choices; if denied, arrays remain empty
+    if (window.confirm("Include lowercase letters?")) {
+      lowerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    }
+
+    if (window.confirm("Include uppercase letters?")) {
+      upperChoices = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    }
+
+    if (window.confirm("Include numbers?")) {
+      numberChoices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    }
+
+    if (window.confirm("Include special characters?")) {
+      specialChoices = [" ", "'", "\"", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "\]", "\[", "^", "_", "`", "{", "|", "}", "~"];
+    }
+
+    //validates that atleast one catagory was chosen
+    if (lowerChoices == 0 && upperChoices == 0 && numberChoices == 0 && specialChoices == 0) {
+      alert("Well you gotta choose one! Lets try again.");
+      return;
+    }
+
+    // array to store random elements pushed from the chosen catagories
+    var userChoices = [];
+
+  //chooses 1 random element from each catagory, storing it it userChoices and looping until desired length is reached
+  //nested while-statements validate whether the catagory they each concern was chosen by checking the array length
+      while (userChoices.length < lengthChoice) {  
+        lower: while (userChoices.length < lengthChoice && lowerChoices.length > 0) {
+            function getRandomLower(max) {
+            return lowerChoices[Math.floor(Math.random() * lowerChoices.length)];
+          };
+          var lowerChoice = getRandomLower();
+          var userChoice = userChoices.push(lowerChoice);
+          if (userChoices.length < lengthChoice) {
+            break;
+          }
+        }
+        upper: while (userChoices.length < lengthChoice && upperChoices.length > 0) { 
+            function getRandomUpper(max) {
+            return upperChoices[Math.floor(Math.random() * upperChoices.length)];
+          }
+          var upperChoice = getRandomUpper();
+          userChoice = userChoices.push(upperChoice);
+          if (userChoices.length < lengthChoice) {
+            break;
+          }
+        }
+        numbers:  while (userChoices.length < lengthChoice && numberChoices.length > 0) { 
+            function getRandomNumber(max) {
+            return numberChoices[Math.floor(Math.random() * numberChoices.length)];
+          }
+          var numberChoice = getRandomNumber();
+          userChoice = userChoices.push(numberChoice);
+          if (userChoices.length < lengthChoice) {
+            break;
+          }
+        }
+        specials: while (userChoices.length < lengthChoice && specialChoices.length > 0) { 
+            function getRandomSpecial(max) {
+            return specialChoices[Math.floor(Math.random() * specialChoices.length)];
+          }
+          var specialChoice = getRandomSpecial();
+          userChoice = userChoices.push(specialChoice);
+          if (userChoices.length < lengthChoice) {
+            break;
+          }
+        }
+       };
+
+      // randomize the random elements (so that the resulting array isn't patterned by catagory)
+       while (password.length < lengthChoice) {
+        function getRandomInt(max) {
+        return [Math.floor(Math.random() * userChoices.length)];
+        }
+        var myInt = getRandomInt()
+        password.push(userChoices[myInt]);
+        userChoices.splice(myInt, 1);
+      }
+    //join the randomized array of elements into a string
+   password = password.join("");
+  // send string back to the writePassword function
+  return password;
+};
